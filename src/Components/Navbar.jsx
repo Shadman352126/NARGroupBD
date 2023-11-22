47f9eb5f4de7476fd8c1abe8ea6a852b5e251d1b
Shadman352126 <shadman352126@gmail.com>
2023-11-22 15:18:00 +0600

    navbar completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 
+// react icons
+import { FaXmark, FaBars } from "react-icons/fa6";
+
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSticky, setIsSticky] = useState(false);
@@ -34,8 +37,14 @@ const Navbar = () => {
 
   return (
     <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
-      <nav>
-        <div>
+      <nav
+        className={`py-4 lg:px-14 px-4 ${
+          isSticky
+            ? "sticky top-0 left-0 right-0 border-b bg-white duration-300"
+            : ""
+        }`}
+      >
+        <div className="flex justify-between items-center text-base gap-8">
           <a
             href=""
             className="text-2xl font-semibold flex items-center space-x-3"
@@ -65,6 +74,49 @@ const Navbar = () => {
           </ul>
 
           {/* button for larger devices */}
+          <div className="space-x-12 hidden lg:flex items-center">
+            <a
+              href="/"
+              className="hidden lg:flex items-center text-brandPrimary hover:text-gray-900"
+            >
+              Login
+            </a>
+            <button className="bg-brandPrimary text-white px-4 py-2 transition-all duration-300 rounded hover:bg-neutralGrey">
+              Sign Up
+            </button>
+          </div>
+          {/* menu button for only mobile devices */}
+          <div className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-neutralGrey focus:outline-none focus:text-gray-500"
+            >
+              {isMenuOpen ? (
+                <FaXmark className="h-6 w-6" />
+              ) : (
+                <FaBars className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+        {/* items for mobile devices */}
+        <div
+          className={`space-y-4 px-4 mt-16 py-7 bg-brandPrimary ${
+            isMenuOpen ? "block fixed top-0 left-0 right-0" : "hidden"
+          }`}
+        >
+          {navItems.map(({ link, path }) => (
+            <Link
+              to={path}
+              spy={true}
+              smooth={true}
+              offset={-100}
+              key={path}
+              className="block text-base text-white hover:text-black first:font-medium"
+            >
+              {link}
+            </Link>
+          ))}
         </div>
       </nav>
     </header>
